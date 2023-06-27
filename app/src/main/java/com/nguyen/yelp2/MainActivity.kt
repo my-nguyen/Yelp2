49082daa7ffd5c21b770224e1b34cc191173b5c2
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-06-26 19:51:49 -0700

    Ep 3: Retrofit Setup- Networking in Kotlin Android Studio

---
+++ app/src/main/java/com/nguyen/yelp2/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.nguyen.yelp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val TAG = "MainActivity"
+private const val API_KEY = "7WnY3zVPZ8Yj9S8JWphAu5dn8myhk0N0eAZ4P5vluMBcEg7t1gc41fdBHgluTHLNziDGBiH0UvciG4-p-IJQfPvR5Pdhd9WJ1G4pQnwZr6_cZG54KU6rZVrjITSfX3Yx"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        val service = retrofit.create(YelpService::class.java)
+        service.searchBusinesses("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object: Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.i(TAG, "onResponse $response")
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/nguyen/yelp2/YelpService.kt
@@ -0,0 +1,11 @@
+package com.nguyen.yelp2
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface YelpService {
+    @GET("businesses/search")
+    fun searchBusinesses(@Header("Authorization") authorization: String, @Query("term") term: String, @Query("location") location: String): Call<Any>
+}
